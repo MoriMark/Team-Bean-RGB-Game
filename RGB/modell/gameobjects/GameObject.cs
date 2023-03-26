9b778cbaac18dd7d22de2477e5470afc39b6f3b2
Móri Márk <dizwyj@INF.ELTE.HU>
2023-03-26 16:55:39 +0200

    Gamobject osztály implementálva

---
+++ RGB/modell/gameobjects/GameObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGB.modell.gameobjects
+{
+    public class GameObject
+    {
+        int i;
+        int j;
+        bool isempty;
+        bool iswall;
+
+        public GameObject(int i, int j)
+        {
+            this.i = i;
+            this.j = j;
+            isempty = false;
+            iswall = false;
+        }
+
+        public bool IsEmpty()
+        {
+            return isempty;
+        }
+
+        public bool IsWall()
+        {
+            return iswall;
+        }
+    }
+}
