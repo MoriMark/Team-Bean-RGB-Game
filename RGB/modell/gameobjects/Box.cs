e80dc30f3badab97b6257dc01088283f08e412f3
Móri Márk <dizwyj@INF.ELTE.HU>
2023-03-26 18:45:28 +0200

    Box class written, missing attaching team enum

---
+++ RGB/modell/gameobjects/Box.cs
@@ -0,0 +1,25 @@
+﻿using RGB.modell.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGB.modell.gameobjects
+{
+    public class Box : GameObject
+    {
+        private int id { get; set; }
+        private static int staticid = 0;
+        private BoxColor color { get; }
+        private int intgroup { get; set; }
+        
+        public Box(int i, int j, BoxColor color) : base(i,j)
+        {
+            id = staticid;
+            staticid++;
+            this.color = color;
+            intgroup = 0;
+        }
+    }
+}
