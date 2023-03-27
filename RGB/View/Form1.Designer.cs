1584dd03bf71c0ff0af7b916446a3daf0f14f3b0
Dávid Nagy <rljp7h@INF.ELTE.HU>
2023-03-27 17:15:44 +0200

    Basic main menu and help windows implementation.

---
+++ RGB/View/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace RGB
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonGameStart = new Button();
+            buttonHelp = new Button();
+            numOfTeams = new NumericUpDown();
+            numericUpDown1 = new NumericUpDown();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)numOfTeams).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // buttonGameStart
+            // 
+            buttonGameStart.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonGameStart.Location = new Point(325, 600);
+            buttonGameStart.Name = "buttonGameStart";
+            buttonGameStart.Size = new Size(250, 50);
+            buttonGameStart.TabIndex = 0;
+            buttonGameStart.Text = "Játék Indítása";
+            buttonGameStart.UseVisualStyleBackColor = true;
+            buttonGameStart.Click += buttonGameStart_Click;
+            // 
+            // buttonHelp
+            // 
+            buttonHelp.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonHelp.Location = new Point(325, 700);
+            buttonHelp.Name = "buttonHelp";
+            buttonHelp.Size = new Size(250, 50);
+            buttonHelp.TabIndex = 1;
+            buttonHelp.Text = "Súgó";
+            buttonHelp.UseVisualStyleBackColor = true;
+            buttonHelp.Click += buttonHelp_Click;
+            // 
+            // numOfTeams
+            // 
+            numOfTeams.Location = new Point(482, 329);
+            numOfTeams.Maximum = new decimal(new int[] { 4, 0, 0, 0 });
+            numOfTeams.Minimum = new decimal(new int[] { 2, 0, 0, 0 });
+            numOfTeams.Name = "numOfTeams";
+            numOfTeams.Size = new Size(55, 23);
+            numOfTeams.TabIndex = 2;
+            numOfTeams.Value = new decimal(new int[] { 2, 0, 0, 0 });
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(482, 276);
+            numericUpDown1.Maximum = new decimal(new int[] { 4, 0, 0, 0 });
+            numericUpDown1.Minimum = new decimal(new int[] { 2, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(55, 23);
+            numericUpDown1.TabIndex = 3;
+            numericUpDown1.Value = new decimal(new int[] { 2, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(325, 276);
+            label1.Name = "label1";
+            label1.Size = new Size(136, 21);
+            label1.TabIndex = 4;
+            label1.Text = "Csapatok száma:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(325, 329);
+            label2.Name = "label2";
+            label2.Size = new Size(131, 21);
+            label2.TabIndex = 5;
+            label2.Text = "Robotok száma:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(884, 961);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(numOfTeams);
+            Controls.Add(buttonHelp);
+            Controls.Add(buttonGameStart);
+            Name = "Form1";
+            Text = "RGB";
+            ((System.ComponentModel.ISupportInitialize)numOfTeams).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonGameStart;
+        private Button buttonHelp;
+        private NumericUpDown numOfTeams;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ RGB/View/GameView.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RGB.View
+{
+    partial class GameView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(52, 619);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 100);
+            button1.TabIndex = 0;
+            button1.Text = "Mozog";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(183, 619);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 100);
+            button2.TabIndex = 1;
+            button2.Text = "Forog";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(315, 619);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 100);
+            button3.TabIndex = 2;
+            button3.Text = "Összekapcsol";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(453, 619);
+            button4.Name = "button4";
+            button4.Size = new Size(100, 100);
+            button4.TabIndex = 3;
+            button4.Text = "Szétkapcsol";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(586, 619);
+            button5.Name = "button5";
+            button5.Size = new Size(100, 100);
+            button5.TabIndex = 4;
+            button5.Text = "Lekapcsol";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Location = new Point(722, 619);
+            button6.Name = "button6";
+            button6.Size = new Size(100, 100);
+            button6.TabIndex = 5;
+            button6.Text = "Felkapcsol";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // GameView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(884, 961);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "GameView";
+            Text = "RGB";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
