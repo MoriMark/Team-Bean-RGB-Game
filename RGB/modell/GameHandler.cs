0e6653ea6864d101fb62bc1318d166447b775347
gaborbozo <bg.bozogabor@gmail.com>
2023-03-30 23:33:33 +0200

    Adding necessary classes

---
+++ RGB/modell/GameHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGB.modell
+{
+    public class GameHandler
+    {
+        private GameRule gameRule;
+
+    }
+}
