6bef0c06713ecc38499d71c8faa751b303cdf640
gaborbozo <bg.bozogabor@gmail.com>
2023-04-02 11:30:41 +0200

    Basic robot group managment

---
+++ RGB/modell/exceptions/RobotNotInTeam.cs
@@ -0,0 +1,13 @@
+﻿using RGB.modell.enums;
+using RGB.modell.gameobjects;
+
+namespace RGB.modell.exceptions
+{
+    public class RobotNotInTeam : Exception
+    {
+        public RobotNotInTeam() { }
+
+        //TODO use robot parameter to identify it
+        public RobotNotInTeam(Robot robot) : base($"Robot not in any team!") { }
+    }
+}
