54688b174e487b093005f5c100245ed27c9e41a6
Bozó Gábor <bg.bozogabor@gmail.com>
2023-04-14 20:46:45 +0200

    Basic Message POCO

---
+++ RGB/modell/Message.cs
@@ -0,0 +1,22 @@
+﻿using RGB.modell.enums;
+using RGB.modell.gameobjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGB.modell
+{
+    public class Message
+    {
+        public readonly Robot robot;
+        public readonly Symbol symbol;
+
+        public Message(Robot robot, Symbol symbol)
+        {
+            this.robot = robot;
+            this.symbol = symbol;
+        }
+    }
+}
