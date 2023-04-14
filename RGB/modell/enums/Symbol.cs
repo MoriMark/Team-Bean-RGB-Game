38447da7ece73b51e43d941a2c6af02773fdb71d
Bozó Gábor <bg.bozogabor@gmail.com>
2023-04-14 20:40:53 +0200

    Adding Symbol enum with values

---
+++ RGB/modell/enums/Symbol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGB.modell.enums
+{
+    public enum Symbol
+    {
+        GoUp, GoDown, GoLeft, GoRight,
+
+        GivingBoxToYou, GivingCaravanToYou, ObligateItFromYou,
+
+        Smiley, Gloomy, Delighted, Authorative,
+    }
+}
