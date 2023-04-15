b53ecac407e509fc371f15a41e2159f9101678f6
Bozó Gábor <bg.bozogabor@gmail.com>
2023-04-15 19:16:56 +0200

    GenerateExits created with temporary final value

--- RGB/modell/game-logic/Field.cs
+++ RGB/modell/game-logic/Field.cs
@@ -1,8 +1,10 @@
 ﻿using RGB.modell.enums;
 using RGB.modell.gameobjects;
+using RGB.modell.structs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +15,8 @@ namespace RGB.modell.game_logic
         private const Int32 border = 5;
         private GameObject[,] field;
         public Int32 TableSize { get; private set; }
+        private List<Exit> exits;
+        private const Int32 numberOfExists = 5;
 
         public Field(Int32 tableSize)
         {
@@ -53,6 +57,82 @@ namespace RGB.modell.game_logic
 
             return calculatedField;
         }
+
+        /// <summary>
+        /// Generate exits on the edges of the board except for the corners
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// Thrown if the given number of exits is less than four or greater than TableSize * 4 - 4 - 4, which represents 
+        /// the maximum number of exits allowed on the board, excluding the corners and duplicated corners calculated by square discrict.
+        /// </exception>
+        public void GenerateExits()
+        {
+            //TableSize * edges - duplicateCorners - corners
+            if (numberOfExists < 4 || numberOfExists > (TableSize * 4 - 4 - 4)) throw new ArgumentException($"NumberOfExists must be greater than 4 and less than or equal {TableSize * 4 - 4 - 4}");
+
+            Random rnd = new Random();
+
+            //Make sure to have one 1-1 exits in every edges
+            {
+                Int32[] rndInd = new int[4];
+                for (int i = 0; i < rndInd.Length; ++i)
+                    //corners excluded with (0,TableSize-1)
+                    rndInd[i] = rnd.Next(1, TableSize-1);
+
+                exits = new List<Exit>(){
+                    new Exit(new Coordinate(0, rndInd[0]),Direction.Up),
+                    new Exit(new Coordinate(rndInd[1], TableSize - 1), Direction.Right),
+                    new Exit(new Coordinate(TableSize - 1, rndInd[2]), Direction.Down),
+                    new Exit(new Coordinate(rndInd[3], 0), Direction.Left)
+                };
+            }
+            int existLeft = numberOfExists - 4;
+            
+            while (existLeft > 0)
+            {
+                Int32 x = 0,y = 0;
+                Direction dir = Direction.Down;
+                //corners excluded with (0,TableSize-1)
+                int rndInd = rnd.Next(1, TableSize-1);
+
+                switch (rnd.Next(0, 4))
+                {
+                    case 0:
+                        x = 0;
+                        y = rndInd;
+                        dir = Direction.Up;
+                        break;
+                    case 1:
+                        x = rndInd;
+                        y = TableSize - 1;
+                        dir = Direction.Right;
+                        break;
+                    case 2:
+                        x = TableSize - 1;
+                        y = rndInd;
+                        dir = Direction.Down;
+                        break;
+                    case 3:
+                        x = rndInd;
+                        y = 0;
+                        dir = Direction.Left;
+                        break;
+                }
+
+                Boolean contains = exits.Exists(e =>
+                    e.Coordinate.X == x &&
+                    e.Coordinate.Y == y &&
+                    //not necessary
+                    e.Direction == dir);
+
+                if (!contains)
+                {
+                    exits.Add(new Exit(new Coordinate(x,y), dir));
+                    existLeft--;
+                }
+            }
+        }
+
         //Field generation for the start of the game
         public List<Robot> GenerateField(Int32 numOfRobots, Int32 numOfTeams)
         {
@@ -173,6 +253,9 @@ namespace RGB.modell.game_logic
                     }
                 }
             }
+
+            GenerateExits();
+
             return robots;
         }
     }
