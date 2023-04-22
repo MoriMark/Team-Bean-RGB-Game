57e6cc365fde9fb1346ba2fcb6ec39c41b5a1e71
Bozó Gábor <bg.bozogabor@gmail.com>
2023-04-22 10:35:34 +0200

    burning default shapes into code

--- RGB/modell/game-logic/TaskHandler.cs
+++ RGB/modell/game-logic/TaskHandler.cs
@@ -2,6 +2,52 @@
 {
     public partial class TaskHandler
     {
-        
+        private static readonly Byte[][,] availableShapes = new Byte[][,]
+        {
+            // 1x2 & 2x1
+            new Byte[,]
+            {
+                { 1,1 },
+            },
+            new Byte[,]
+            {
+                { 1 },
+                { 1 }
+            },
+
+            // 2x2
+            new Byte[,]
+            {
+                { 1,1 },
+                { 1,1 }
+            },
+
+            // 3x2 & 2x3
+            new Byte[,]{
+                { 1,0 },
+                { 1,0 },
+                { 1,1 }
+            },
+            new Byte[,]{
+                { 1,1 },
+                { 0,1 },
+                { 0,1 }
+            },
+            new Byte[,]{
+                { 1,0,0 },
+                { 1,1,1 },
+            },
+            new Byte[,]{
+                { 1,1,1 },
+                { 0,0,1 },
+            },
+
+            // 3x3
+            new Byte[,]{
+                { 0,1,0 },
+                { 1,1,1 },
+                { 0,1,0 }
+            },
+        };
     }
 }
