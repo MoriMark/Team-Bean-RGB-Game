68c2d396e616f9e641aa46047b0f4febb05137be
Bozó Gábor <bg.bozogabor@gmail.com>
2023-04-22 10:42:34 +0200

    Fixing incorrect reference in UpdateTasksEventArgs and create necessary functions for it in TaskHandler

--- RGB/modell/game-logic/TaskHandler.cs
+++ RGB/modell/game-logic/TaskHandler.cs
@@ -1,7 +1,13 @@
-﻿namespace RGB.modell.game_logic
+﻿using RGB.modell.enums;
+using RGB.modell.events;
+using Task = RGB.modell.structs.Task;
+
+namespace RGB.modell.game_logic
 {
     public partial class TaskHandler
     {
+        Dictionary<Team, List<Task>> tasks;
+
         private static readonly Byte[][,] availableShapes = new Byte[][,]
         {
             // 1x2 & 2x1
@@ -49,5 +55,18 @@
                 { 0,1,0 }
             },
         };
+
+        public event EventHandler<UpdateTasksEventArgs> UpdateFields;
+
+        public TaskHandler()
+        {
+            tasks = new Dictionary<Team, List<Task>>();
+        }
+
+        public void OnTasksUpdate(Team team)
+        {
+            if (UpdateFields != null)
+                UpdateFields(this, new UpdateTasksEventArgs(new List<Task>(tasks[team])));
+        }
     }
 }
