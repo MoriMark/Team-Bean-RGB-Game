9d88fc2390201afa1ca24c8f8f17e46a005cd5a1
Bozó Gábor <bg.bozogabor@gmail.com>
2023-04-22 10:33:13 +0200

    Basic EventArgs for TaskHandler

---
+++ RGB/modell/events/UpdateTasksEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGB.modell.events
+{
+    public class UpdateTasksEventArgs : EventArgs
+    {
+        public List<Task> tasks;
+
+        public UpdateTasksEventArgs(List<Task> tasks)
+        {
+            this.tasks = tasks;
+        }
+    }
+}
