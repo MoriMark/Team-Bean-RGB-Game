d05db443785d982c634de23542b8bbf5d7744eb8
Dávid Nagy <rljp7h@INF.ELTE.HU>
2023-05-08 22:19:06 +0200

    Logo added with a test animation

--- RGB/View/Form1.Designer.cs
+++ RGB/View/Form1.Designer.cs
@@ -35,20 +35,22 @@
             label1 = new Label();
             label2 = new Label();
             tableLayoutPanel2 = new TableLayoutPanel();
-            tableLayoutPanel3 = new TableLayoutPanel();
             tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            logoPicture = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)numOfRobots).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numOfTeams).BeginInit();
             tableLayoutPanel2.SuspendLayout();
-            tableLayoutPanel3.SuspendLayout();
             tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)logoPicture).BeginInit();
             SuspendLayout();
             // 
             // buttonGameStart
             // 
             buttonGameStart.Anchor = AnchorStyles.None;
             buttonGameStart.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonGameStart.Location = new Point(317, 640);
+            buttonGameStart.Location = new Point(203, 464);
             buttonGameStart.Name = "buttonGameStart";
             buttonGameStart.Size = new Size(250, 50);
             buttonGameStart.TabIndex = 0;
@@ -60,7 +62,7 @@
             // 
             buttonHelp.Anchor = AnchorStyles.Top;
             buttonHelp.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonHelp.Location = new Point(317, 738);
+            buttonHelp.Location = new Point(203, 562);
             buttonHelp.Name = "buttonHelp";
             buttonHelp.Size = new Size(250, 50);
             buttonHelp.TabIndex = 1;
@@ -72,7 +74,7 @@
             // 
             numOfRobots.Anchor = AnchorStyles.Left;
             numOfRobots.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            numOfRobots.Location = new Point(486, 35);
+            numOfRobots.Location = new Point(362, 35);
             numOfRobots.Margin = new Padding(10);
             numOfRobots.Maximum = new decimal(new int[] { 4, 0, 0, 0 });
             numOfRobots.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
@@ -85,7 +87,7 @@
             // 
             numOfTeams.Anchor = AnchorStyles.Left;
             numOfTeams.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            numOfTeams.Location = new Point(486, 35);
+            numOfTeams.Location = new Point(362, 35);
             numOfTeams.Margin = new Padding(10);
             numOfTeams.Maximum = new decimal(new int[] { 4, 0, 0, 0 });
             numOfTeams.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
@@ -99,7 +101,7 @@
             label1.Anchor = AnchorStyles.Right;
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(330, 39);
+            label1.Location = new Point(206, 39);
             label1.Margin = new Padding(10);
             label1.Name = "label1";
             label1.Size = new Size(136, 21);
@@ -111,7 +113,7 @@
             label2.Anchor = AnchorStyles.Right;
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(335, 39);
+            label2.Location = new Point(211, 39);
             label2.Margin = new Padding(10);
             label2.Name = "label2";
             label2.Size = new Size(131, 21);
@@ -126,31 +128,15 @@
             tableLayoutPanel2.Controls.Add(tableLayoutPanel3, 0, 0);
             tableLayoutPanel2.Controls.Add(buttonHelp, 0, 3);
             tableLayoutPanel2.Controls.Add(buttonGameStart, 0, 2);
-            tableLayoutPanel2.Dock = DockStyle.Fill;
-            tableLayoutPanel2.Location = new Point(0, 0);
+            tableLayoutPanel2.Location = new Point(101, 71);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 4;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 52.20949F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 47.79051F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 140F));
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 225F));
-            tableLayoutPanel2.Size = new Size(884, 961);
+            tableLayoutPanel2.Size = new Size(657, 785);
             tableLayoutPanel2.TabIndex = 7;
-            // 
-            // tableLayoutPanel3
-            // 
-            tableLayoutPanel3.ColumnCount = 2;
-            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 54.2141228F));
-            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45.7858772F));
-            tableLayoutPanel3.Controls.Add(label1, 0, 0);
-            tableLayoutPanel3.Controls.Add(numOfTeams, 1, 0);
-            tableLayoutPanel3.Dock = DockStyle.Bottom;
-            tableLayoutPanel3.Location = new Point(3, 208);
-            tableLayoutPanel3.Name = "tableLayoutPanel3";
-            tableLayoutPanel3.RowCount = 1;
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel3.Size = new Size(878, 100);
-            tableLayoutPanel3.TabIndex = 8;
             // 
             // tableLayoutPanel1
             // 
@@ -160,29 +146,58 @@
             tableLayoutPanel1.Controls.Add(numOfRobots, 1, 0);
             tableLayoutPanel1.Controls.Add(label2, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Top;
-            tableLayoutPanel1.Location = new Point(3, 314);
+            tableLayoutPanel1.Location = new Point(3, 222);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(878, 100);
+            tableLayoutPanel1.Size = new Size(651, 100);
             tableLayoutPanel1.TabIndex = 8;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 2;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 54.2141228F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45.7858772F));
+            tableLayoutPanel3.Controls.Add(label1, 0, 0);
+            tableLayoutPanel3.Controls.Add(numOfTeams, 1, 0);
+            tableLayoutPanel3.Dock = DockStyle.Bottom;
+            tableLayoutPanel3.Location = new Point(3, 116);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 1;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel3.Size = new Size(651, 100);
+            tableLayoutPanel3.TabIndex = 8;
+            // 
+            // logoPicture
+            // 
+            logoPicture.BackColor = Color.Transparent;
+            logoPicture.BackgroundImage = Properties.Resources.teambeanlogo_cropped;
+            logoPicture.BackgroundImageLayout = ImageLayout.Zoom;
+            logoPicture.Location = new Point(0, 0);
+            logoPicture.Name = "logoPicture";
+            logoPicture.Size = new Size(890, 969);
+            logoPicture.TabIndex = 10;
+            logoPicture.TabStop = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(884, 961);
+            Controls.Add(logoPicture);
             Controls.Add(tableLayoutPanel2);
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "Form1";
             Text = "RGB";
             ((System.ComponentModel.ISupportInitialize)numOfRobots).EndInit();
             ((System.ComponentModel.ISupportInitialize)numOfTeams).EndInit();
             tableLayoutPanel2.ResumeLayout(false);
-            tableLayoutPanel3.ResumeLayout(false);
-            tableLayoutPanel3.PerformLayout();
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)logoPicture).EndInit();
             ResumeLayout(false);
         }
 
@@ -197,5 +212,6 @@
         private TableLayoutPanel tableLayoutPanel2;
         private TableLayoutPanel tableLayoutPanel1;
         private TableLayoutPanel tableLayoutPanel3;
+        private PictureBox logoPicture;
     }
 }
--- RGB/View/Form1.cs
+++ RGB/View/Form1.cs
@@ -1,4 +1,5 @@
 using RGB.View;
+using Timer = System.Windows.Forms.Timer;
 
 namespace RGB
 {
@@ -6,12 +7,20 @@ namespace RGB
     {
         private static HelpForm help = null!;
         private static GameView gameView = null!;
+        private Timer animTimer;
+        private int animTime = 1000;
+        private int waitTime = 1000;
+        private int animFrame = 0;
 
         public Form1()
         {
             InitializeComponent();
             help = new HelpForm();
 
+            animTimer = new Timer();
+            animTimer.Interval = 16;
+            animTimer.Tick += Animate;
+            animTimer.Start();
         }
 
         private void buttonHelp_Click(object sender, EventArgs e)
@@ -33,6 +42,29 @@ namespace RGB
             gameView?.Dispose();
             gameView = null!;
             this.Show();
+        }
+
+        private void Animate(object? sender, EventArgs e)
+        {
+            if (waitTime > 0)
+            {
+                waitTime -= 16;
+            }
+            else if (waitTime < 0)
+            {
+                if (animTime > 0)
+                {
+                    animFrame += 1;
+                    animTime -= 16;
+                    logoPicture.Location = new Point(animFrame * (this.Size.Width / 60), 0);
+                }
+                else
+                {
+                    animTimer.Stop();
+                    logoPicture.Visible = false;
+                }
+            }
+
         }
     }
 }
